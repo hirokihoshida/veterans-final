cdab9ea7a73b1996bd59132b8555edb782606f3c
Matt <matthewpark319@gmail.com>
2017-02-27 12:36:32 -0500

    created migration

--- resources/views/addclient.blade.php
+++ resources/views/addclient.blade.php
@@ -1,8 +1,30 @@
 <html>
-    <body>
-        <h1>Add New Client</h1>
-        <form>
+    <head>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <style>
+            .form-element {
+                display: block;
+                padding: 10px;
+            }
 
-        </form>
+            .text-field {
+                display: inline;
+            }
+        </style>
+    </head>
+    <body>
+        <h1 class="a">Add New Client</h1>
+        <h3>Fill out all the fields to add a new veteran client to the database.</h3>
+        <div id="form-container">
+            <form method="post">
+
+                <div class="form-element">
+                    <p class="text-field">First Name: </p><input class="text-field" type="text" name="first_name" required="required">
+                </div>
+                <div class="form-element">
+                    <p class="text-field">Last Name: </p><input class="text-field" type="text" name="last_name" required="required">
+                </div>
+            </form>
+        </div>
     </body>
 </html>
